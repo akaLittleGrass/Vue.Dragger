504a12391323e46234c9aacced2c9514fecd107a
Haoyang Wei <weihaoyang@ainnovation.com>
2020-02-04 18:01:35 +0800

    add jest

---
+++ tests/unit/basic.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount, mount, createWrapper } from '@vue/test-utils';
+import VueDragger from '@/components/VueDragger';
+
+describe('vueDragger component', () => {
+  it('mount the component', () => {
+    //coming soon
+  })
+})
